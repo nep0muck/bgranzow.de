837fc64cf5ebbd1c8a3fbaf248d43c06592621a5
nep0muck <me@eichlersolutions.de>
2016-02-25 15:40:30 +0100

    Redesign of navbar

--- js/functions.js
+++ js/functions.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
+    // Navbar shrinking
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+        $('.sitebrand-header-portrait').addClass('shrink-logo');
+      } else {
+        $('nav').removeClass('shrink');
+        $('.sitebrand-header-portrait').removeClass('shrink-logo');
+      }
+    });
+
 });
